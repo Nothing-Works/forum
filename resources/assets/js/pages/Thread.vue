f3deea0ceef191d4a7b39fec910c9d22efc6ce2c
Andy Song <andylauszp@163.com>
2017-10-17 22:59:19 +1300

    first forum draft

---
+++ resources/assets/js/pages/Thread.vue
@@ -0,0 +1,27 @@
+<script>
+    import Replies from '../components/Replies.vue';
+    import SubscribeButton from '../components/SubscribeButton.vue';
+
+    export default {
+        props: ['thread'],
+
+        components: {Replies, SubscribeButton},
+
+        data () {
+            return {
+                repliesCount: this.thread.replies_count,
+                locked: this.thread.locked
+            };
+        },
+
+        methods: {
+            toggleLock () {
+                axios[
+                    this.locked ? 'delete' : 'post'
+                ]('/locked-threads/' + this.thread.slug);
+
+                this.locked = ! this.locked;
+            }
+        }
+    }
+</script>
